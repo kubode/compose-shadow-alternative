071e7b2a10413f255b5c81b2357a85e027e6c54d
Masatoshi Kubode <swi.masatoshi@gmail.com>
2023-11-02 19:53:54 +0900

    Add targets

--- compose-drop-shadow/build.gradle.kts
+++ compose-drop-shadow/build.gradle.kts
@@ -1,8 +1,17 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.android.library)
 }
 
 kotlin {
     jvm()
+    androidTarget()
+    iosArm64()
+    iosX64()
+    iosSimulatorArm64()
+}
+
+android {
+    namespace = "com.github.kubode.compose.dropshadow"
 }
