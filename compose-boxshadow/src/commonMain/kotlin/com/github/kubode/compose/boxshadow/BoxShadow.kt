3a2aa245e3ee2af7729bc97198270d313954bf03
Masatoshi Kubode <swi.masatoshi@gmail.com>
2023-11-04 22:06:34 +0900

    Add modifier

--- compose-boxshadow/src/commonMain/kotlin/com/github/kubode/compose/boxshadow/BoxShadow.kt
+++ compose-boxshadow/src/commonMain/kotlin/com/github/kubode/compose/boxshadow/BoxShadow.kt
@@ -6,10 +6,21 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.Dp
 
+/**
+ * Modifier to apply a box shadow.
+ *
+ * @param color The color of the shadow.
+ * @param dx The horizontal offset of the shadow.
+ * @param dy The vertical offset of the shadow.
+ * @param radius The blur radius of the shadow.
+ * @param modifier The modifier to apply to this layout node.
+ * @param shape The shape of the shadow.
+ */
 public fun Modifier.boxShadow(
     color: Color,
     dx: Dp,
     dy: Dp,
     radius: Dp,
+    modifier: Modifier = Modifier,
     shape: Shape = RectangleShape,
 ): Modifier = this
