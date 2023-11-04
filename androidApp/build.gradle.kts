de41ef3f958800d0b61b035794ef0d86d397ccc5
Masatoshi Kubode <swi.masatoshi@gmail.com>
2023-11-04 16:19:37 +0900

    Add launcher icon

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     compileSdk = 34
     defaultConfig {
         applicationId = "com.github.kubode.compose.shadow.android"
-        minSdk = 21
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
