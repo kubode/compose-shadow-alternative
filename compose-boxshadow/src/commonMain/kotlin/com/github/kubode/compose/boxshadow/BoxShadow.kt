b544e65234e2966efb26edc5111dca3f7d696d57
Masatoshi Kubode <swi.masatoshi@gmail.com>
2023-11-04 22:12:47 +0900

    Replace dx and dy with offset

--- compose-boxshadow/src/commonMain/kotlin/com/github/kubode/compose/boxshadow/BoxShadow.kt
+++ compose-boxshadow/src/commonMain/kotlin/com/github/kubode/compose/boxshadow/BoxShadow.kt
@@ -5,21 +5,20 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpOffset
 
 /**
  * Modifier to apply a box shadow.
  *
  * @param color The color of the shadow.
- * @param dx The horizontal offset of the shadow.
- * @param dy The vertical offset of the shadow.
+ * @param offset The offset of the shadow.
  * @param radius The blur radius of the shadow.
  * @param modifier The modifier to apply to this layout node.
  * @param shape The shape of the shadow.
  */
 public fun Modifier.boxShadow(
     color: Color,
-    dx: Dp,
-    dy: Dp,
+    offset: DpOffset,
     radius: Dp,
     modifier: Modifier = Modifier,
     shape: Shape = RectangleShape,
