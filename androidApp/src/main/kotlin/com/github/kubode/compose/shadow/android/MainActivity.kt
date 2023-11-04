830834ba0dc7b75b2e17de567089af2d0367194f
Masatoshi Kubode <swi.masatoshi@gmail.com>
2023-11-04 16:23:20 +0900

    Fix UI not shown

--- androidApp/src/main/kotlin/com/github/kubode/compose/shadow/android/MainActivity.kt
+++ androidApp/src/main/kotlin/com/github/kubode/compose/shadow/android/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.github.kubode.compose.shadow.android
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
@@ -18,12 +18,13 @@ import androidx.compose.ui.unit.dp
 import com.github.kubode.compose.dropshadow.DropShadow
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContent {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
+                    .background(Color.White)
                     .padding(16.dp)
                     .scrollable(
                         state = rememberScrollState(),
